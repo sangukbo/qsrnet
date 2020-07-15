c73aa842a31327c1cc2c8cfe1b03cd9e3b5de272
Sang Uk Lee <sangukbo@mit.edu>
2020-07-15 13:55:14 -0400

    dbn part

--- qsrnet/utils/etc.py
+++ qsrnet/utils/etc.py
@@ -6,6 +6,19 @@ def get_object_ids(object_names, class_names):
         object_ids.append(class_names.index(object_name))
     return object_ids
 
+def get_pair_ids(pair_names, class_names):
+    pair_ids = []
+    for pair_name in pair_names:
+        obj1_name, obj2_name = pair_name[1:-1].split(',')
+        obj1_id = class_names.index(obj1_name); obj2_id = class_names.index(obj2_name)
+        pair_ids.append('(' + str(obj1_id) + ',' + str(obj2_id) + ')')
+    return pair_ids
+
+def pair_name_from_id(paid_id, class_names):
+    obj1_id, obj2_id = pair_id[1:-1].split(',')
+    obj1_name = class_names[obj1_id]; obj2_name = class_names[obj2_id]
+    return '(' + obj1_name + ',' + obj2_name + ')'
+
 def masks_for_objects_of_interest(mask_results, object_ids):
     # compute new mask_results for objects in object_ids
     final_size = 0
