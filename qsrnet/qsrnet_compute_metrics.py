58cc097e0350998c0b25192e96aaff9ca5a48675
Sang Uk Lee <sangukbo@mit.edu>
2020-07-16 12:41:12 -0400

    outputs image

--- qsrnet/qsrnet_compute_metrics.py
+++ qsrnet/qsrnet_compute_metrics.py
@@ -61,6 +61,9 @@ def compute_metrics_main():
             time_for_single_iteration = time.time()
             depth_image, color_image = realsense.get_realsense_images()
 
+            cv2.imwrite('/home/appuser/qsrnet/data_output/image/color/image' + str(iteration) + '.jpg', color_image)
+            cv2.imwrite('/home/appuser/qsrnet/data_output/image/depth/image' + str(iteration) + '.png', depth_image)
+
             # mask rcnn
             mask_results = maskrcnn_unit.compute_masks(color_image)
             # dense pose
@@ -69,8 +72,17 @@ def compute_metrics_main():
             mask_results['masks'] = np.append(mask_results['masks'], body_part_masks, axis = 0)
             mask_results['rois'] = np.append(mask_results['rois'], body_part_rois, axis = 0)
             mask_results = masks_for_objects_of_interest(mask_results, object_ids)
+
+            image_masks = image_with_masks(color_image, mask_results['masks'])
+            cv2.imwrite('/home/appuser/qsrnet/data_output/image/mask/image' + str(iteration) + '.jpg', image_masks)
+
             # find point cloud
             pcd_dict = construct_point_cloud_dict(mask_results, color_image, depth_image, configuration)
+            point_cloud_masks = point_cloud_with_masks(pcd_dict, depth_image, configuration)
+            o3d.io.write_point_cloud('/home/appuser/qsrnet/data_output/point_cloud/mask/point_cloud' + str(iteration) + '.pcd', point_cloud_masks)
+            # pcd = o3d.io.read_point_cloud('/home/sanguk/Desktop/qsrnet/data_output/point_cloud/mask/point_cloud0.pcd')
+            # o3d.visualization.draw_geometries([pcd])
+
             # compute metrics
             metric_dict, previous_center_dict, timestamp = compute_metrics_from_point_clouds(pcd_dict, object_ids, pair_ids, timestamp, previous_center_dict)
 
