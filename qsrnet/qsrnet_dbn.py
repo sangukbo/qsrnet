44363e347581882a4309ee437e273b854c8a54ac
Sang Uk Lee <sangukbo@mit.edu>
2020-07-16 17:10:26 -0400

    metric qsr csv

--- qsrnet/qsrnet_dbn.py
+++ qsrnet/qsrnet_dbn.py
@@ -21,9 +21,7 @@ object_ids = get_object_ids(configuration['NAMES']['object_names'], configuratio
 pair_ids = get_pair_ids(configuration['NAMES']['pair_names'], configuration['NAMES']['class_names'])
 
 for pair_id_csv in pair_ids:
-    open(configuration['DIRECTORIES']['qsrnet_dir'] + '/data_output/metric/' + 'metric_' + pair_id_csv + '.csv', 'w')
-for pair_id_csv in pair_ids:
-    open(configuration['DIRECTORIES']['qsrnet_dir'] + '/data_output/qsr/' + 'qsr_' + pair_id_csv + '.csv', 'w')
+    open(configuration['DIRECTORIES']['qsrnet_dir'] + '/data_output/' + 'metric_qsr_' + pair_id_csv + '.csv', 'w')
 
 def dbn_qsr_main():
     pf_iteration = 0
@@ -48,22 +46,6 @@ def dbn_qsr_main():
             if not packet: break
             metric_msg.append(packet)
         metrics = pickle.loads(b"".join(metric_msg))
-        print(metrics)
-        # save metric
-        if metrics['velocity1'] != {}:
-            for pair_id in pair_ids:
-                with open(configuration['DIRECTORIES']['qsrnet_dir'] + '/data_output/metric/' + 'metric_' + pair_id + '.csv', 'a') as f:
-                    field_name = ['pcd distance', 'center distance', 'velocity 1', 'velocity 2', 'velocity 3']
-                    metric_dict = {}
-                    metric_dict['pcd distance'] = metrics['pcd_distances'][pair_id]
-                    metric_dict['center distance'] = metrics['center_distances'][pair_id]
-                    metric_dict['velocity 1'] = metrics['velocity1'][pair_id]
-                    metric_dict['velocity 2'] = metrics['velocity2'][pair_id]
-                    metric_dict['velocity 3'] = metrics['velocity3'][pair_id]
-                    writer = csv.DictWriter(f, fieldnames=field_name)
-                    writer.writerow(metric_dict)
-        with open(configuration['DIRECTORIES']['qsrnet_dir'] + '/data_output/metric/' + 'metric_' + str(pf_iteration) + '.pickle', 'wb') as f:
-            pickle.dump(metrics, f, protocol=2)
         client_socket.close()
 
         timestamp = time.time()
@@ -74,23 +56,27 @@ def dbn_qsr_main():
             if (pf_iteration+1) % configuration['ARGUMENTS']['resample_at'] == 0:
                 qsr_pf_dict[pair_id].resample()
             qsr_joint_max_state = qsr_pf_dict[pair_id].compute_max_joint()
+            qsr_joint_max_state_dict[pair_id] = qsr_joint_max_state
             """
             elementwise_max_state_dict = pf_qrk.compute_max_elementwise()
             with open(main_dir + '/' + 'qsr/elementwise/' + 'qsr' + str(time_step + args.pre_num) + '.pickle', 'wb') as f:
                 pickle.dump(elementwise_max_state_dict, f, protocol=2)
             """
-            with open(configuration['DIRECTORIES']['qsrnet_dir'] + '/data_output/qsr/' + 'qsr_' + pair_id + '.csv', 'a') as f:
-                field_name = ['rcc', 'qtc1', 'qtc2', 'qtc3', 'qdc', 'probability']
-                qsr_dict = {}
-                qsr_dict['rcc'] = qsr_joint_max_state['state']['rcc']
-                qsr_dict['qtc1'] = qsr_joint_max_state['state']['qtc1']
-                qsr_dict['qtc2'] = qsr_joint_max_state['state']['qtc2']
-                qsr_dict['qtc3'] = qsr_joint_max_state['state']['qtc3']
-                qsr_dict['qdc'] = qsr_joint_max_state['state']['qdc']
-                qsr_dict['probability'] = qsr_joint_max_state['probability']
-                writer = csv.DictWriter(f, fieldnames=field_name)
-                writer.writerow(qsr_dict)
-            qsr_joint_max_state_dict[pair_id] = qsr_joint_max_state
+            if metrics['velocity1'] != {}:
+                with open(configuration['DIRECTORIES']['qsrnet_dir'] + '/data_output/' + 'metric_qsr_' + pair_id + '.csv', 'a') as f:
+                    field_name = ['pcd distance', 'center distance', 'velocity 1', 'velocity 2', 'velocity 3', 'rcc', 'qtc1', 'qtc2', 'qtc3', 'qdc', 'probability']
+                    metric_qsr_dict = {}
+                    metric_qsr_dict['pcd distance'] = metrics['pcd_distances'][pair_id]; metric_qsr_dict['center distance'] = metrics['center_distances'][pair_id]; metric_qsr_dict['velocity 1'] = metrics['velocity1'][pair_id]
+                    metric_qsr_dict['velocity 2'] = metrics['velocity2'][pair_id]; metric_qsr_dict['velocity 3'] = metrics['velocity3'][pair_id]
+                    metric_qsr_dict['rcc'] = qsr_joint_max_state['state']['rcc']; metric_qsr_dict['qtc1'] = qsr_joint_max_state['state']['qtc1']; metric_qsr_dict['qtc2'] = qsr_joint_max_state['state']['qtc2']
+                    metric_qsr_dict['qtc3'] = qsr_joint_max_state['state']['qtc3']; metric_qsr_dict['qdc'] = qsr_joint_max_state['state']['qdc']; metric_qsr_dict['probability'] = qsr_joint_max_state['probability']
+                    writer = csv.DictWriter(f, fieldnames=field_name)
+                    writer.writerow(metric_qsr_dict)
+        # save metric
+        print(metrics)
+        with open(configuration['DIRECTORIES']['qsrnet_dir'] + '/data_output/metric/' + 'metric_' + str(pf_iteration) + '.pickle', 'wb') as f:
+            pickle.dump(metrics, f, protocol=2)
+        # save qsr
         print(qsr_joint_max_state_dict)
         with open(configuration['DIRECTORIES']['qsrnet_dir'] + '/data_output/qsr/' + 'qsr_' + str(pf_iteration) + '.pickle', 'wb') as f:
             pickle.dump(qsr_joint_max_state_dict, f, protocol=2)
