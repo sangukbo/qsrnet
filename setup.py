9bfd932851c72b093914cd9481c05c8c4628f1c8
Sang Uk Lee <sangukbo@mit.edu>
2020-11-07 02:08:09 -0500

    fix error

--- setup.py
+++ setup.py
@@ -2,15 +2,10 @@
 
 from setuptools import setup, find_packages
 
-
-with open('README.md') as f:
-    readme = f.read()
-
 setup(
     name='qsrnet',
     version='0.1.0',
     description='qsrnet',
-    long_description=readme,
     author='Sang Uk Lee',
     author_email='sangukbo@mit.edu',
     url='https://github.com/sangukbo/qsrnet',
